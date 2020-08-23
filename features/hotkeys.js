50df15c848de7e8c03fabe9d83619b700f464889
Roman Klimenko <romaklimenko@gmail.com>
2020-08-23 14:16:41 +0200

    горячие клавиши

---
+++ features/hotkeys.js
@@ -0,0 +1,51 @@
+// горячие клавиши:
+// - H - главная (подписки)
+// - U - профиль пользователя
+// - M - моё
+// - I - инбокс
+// - D - черновики
+(() => {
+  document.body.addEventListener('keydown', (event) => {
+
+    if (event.target.tagName.toUpperCase() === 'TEXTAREA' || event.target.tagName.toUpperCase() === 'INPUT') {
+      return;
+    }
+
+    if (event.target.attributes['contenteditable']) {
+      return;
+    }
+
+    console.log(event);
+
+    switch (event.code) {
+      case 'KeyH':
+        window.location.href = 'https://d3.ru/';
+        break;
+      case 'KeyI':
+        window.location.href = 'https://d3.ru/my/inbox/';
+        break;
+      case 'KeyM':
+        window.location.href = 'https://d3.ru/my/';
+        break;
+      case 'KeyD':
+        window.location.href = 'https://d3.ru/drafts/';
+        break;
+      case 'KeyU':
+        let username = localStorage['user_login'];
+
+        if (!username) {
+          const a = document.querySelector('a.s-menu__button-user-login');
+          if (a) {
+            username = a.innerText;
+          }
+        }
+
+        if (username) {
+          window.location.href = `https://d3.ru/user/${username}/`;
+        }
+        break;
+      default:
+        break;
+    }
+  });
+})();
