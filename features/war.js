a5136bea215fc78e27fa59f245f5c3ba1f7770a1
romaklimenko <romaklimenko@gmail.com>
2022-03-12 20:30:03 +0100

    manifest v2 -> v3 check after a timeout if the logo is not available immediately

--- features/war.js
+++ features/war.js
@@ -1,13 +1,23 @@
 (() => {
+  if (!sorrow()) {
+    setTimeout(sorrow, 5000);
+  }
+})();
+
+function sorrow() {
   const sorrowUrl = 'https://storage.googleapis.com/dirty-public/logo_main_retina_black.png';
   const jsLogo = document.getElementById('js-logo');
   if (jsLogo) {
     jsLogo.style.backgroundImage = `url(${sorrowUrl})`;
+    return true;
   }
 
   const domainLogo = document.getElementsByClassName('s-header__domain-logo');
 
   if (domainLogo.length > 0) {
     domainLogo[0].style.backgroundImage = `url(${sorrowUrl})`;
+    return true;
   }
-})();
+
+  return false;
+}
